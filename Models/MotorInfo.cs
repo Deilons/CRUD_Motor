4e1039cc0151bd1460259c6db290eb11a3e47825
Deilons <apolo4748@gmail.com>
2024-08-28 07:45:00 -0500

    Feat: data anotations to motorInfo

--- Models/MotorInfo.cs
+++ Models/MotorInfo.cs
@@ -1,37 +1,69 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
 
-namespace CRUD_Motor.Models;
-
-[Table("motor_info")]
-public class MotorInfo
+namespace CRUD_Motor.Models
 {
-    [Key]
-    [Column("id")]
-    public Guid Id { get; set; }
+    [Table("motor_info")]
+    public class MotorInfo
+    {
+        [Key]
+        [Column("id")]
+        public Guid Id { get; set; }
 
-    public string Brand { get; set; }
+        [Required]
+        [Column("brand")]
+        [StringLength(50)]
+        public string Brand { get; set; }
 
-    public string Model { get; set; }
+        [Required]
+        [Column("model")]
+        [StringLength(50)]
+        public string Model { get; set; }
 
-    public float Displacement { get; set; }
+        [Required]
+        [Column("displacement")]
+        [Range(0.1, 10.0)]
+        public float Displacement { get; set; }
 
-    public int Cylinders { get; set; }
+        [Required]
+        [Column("cylinders")]
+        [Range(1, 16)]
+        public int Cylinders { get; set; }
 
-    public string Aspiration { get; set; }
+        [Required]
+        [Column("aspiration")]
+        [StringLength(20)]
+        public string Aspiration { get; set; }
 
-    public int HorsePower { get; set; }
+        [Required]
+        [Column("horse_power")]
+        [Range(0, 2000)]
+        public int HorsePower { get; set; }
 
-    public int Torque { get; set; }
+        [Required]
+        [Column("torque")]
+        [Range(0, 2000)]
+        public int Torque { get; set; }
 
-    public enum FuelType { Gasoline, Diesel, Electric}
+        [Required]
+        [Column("fuel_type")]
+        public FuelTypeEnum FuelType { get; set; }
 
-    public bool IsTurbocharged { get; set; }
+        [Required]
+        [Column("is_turbocharged")]
+        public bool IsTurbocharged { get; set; }
 
-    public int Year { get; set; }
+        [Required]
+        [Column("year")]
+        [Range(1886, 2100)]  // Considerando que el primer automóvil se fabricó en 1886
+        public int Year { get; set; }
+    }
+
+    public enum FuelTypeEnum
+    {
+        Gasoline,
+        Diesel,
+        Electric
+    }
 }
